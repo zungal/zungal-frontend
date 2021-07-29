243b68acb8021e2bc44350bb96d3cf1b63a0d548
zungal <88108243+zungal@users.noreply.github.com>
2021-07-29 19:21:41 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import{Router as BrowserRouter,Switch,Route} from 'react-router-dom';
+import Login from './components/Login';
+function App() {
+  return (
+    <div className="App">
+      <Login />
+    </div>
+  );
+}
+
+export default App;
